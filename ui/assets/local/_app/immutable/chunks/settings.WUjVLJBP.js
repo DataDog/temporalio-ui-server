1c0ff0c11cd1c5fecd1b31b7f2a18647984b3c39
Sonal Parab <sonal.parab@datadoghq.com>
2025-05-28 13:41:22 +0530

    Update UI Server

--- ui/assets/local/_app/immutable/chunks/settings.WUjVLJBP.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./entry.5aDRfZRw.js";import{p as a}from"./stores.94z3PgvV.js";const r=t([a],([s])=>s.data.settings);export{r as s};
--- ui/assets/local/_app/immutable/entry/start.piBFGZQh.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"../chunks/entry.5aDRfZRw.js";export{a as start};
