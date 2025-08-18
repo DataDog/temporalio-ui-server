782e3a3f4845f628726118450cdec19cd75106fb
Hardy Ferentschik <hardy@ferentschik.de>
2025-08-18 13:42:40 +0200

    Update UI Server

--- server/route/ui.go
+++ server/route/ui.go
@@ -24,20 +24,12 @@ package route
 
 import (
 	"bytes"
-	"crypto/rand"
-	"encoding/hex"
 	"fmt"
-	"html/template"
 	"io/fs"
 	"log"
 	"net/http"
-	"path"
 	"regexp"
 	"strings"
-
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
 
 	"github.com/labstack/echo/v4"
 )
