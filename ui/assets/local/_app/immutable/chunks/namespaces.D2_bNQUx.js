ddd723c58fcdc7bbb2ce91f4554a1a7869126e49
Hardy Ferentschik <hardy@ferentschik.de>
2025-09-01 11:27:57 +0200

    Update UI Server

---
+++ ui/assets/local/_app/immutable/chunks/namespaces.D2_bNQUx.js
@@ -0,0 +1,1 @@
+import{w as s}from"./entry.DmdN9037.js";import{p as a}from"./persist-store.BoJ_SRcc.js";const r=a("lastNamespace","default",!0),o=s([]);export{r as l,o as n};
--- ui/assets/local/_app/immutable/chunks/namespaces.D3hg2smR.js
+++
@@ -1,1 +0,0 @@
-import{w as s}from"./entry.Dyt5zPUK.js";import{p as a}from"./persist-store.AR1Y9MuQ.js";const r=a("lastNamespace","default",!0),o=s([]);export{r as l,o as n};
